693fc28d07ffc0300627d6a2aba7283288bb7fc6
nikkscode <dominik.kisiala@gmail.com>
2022-05-12 14:52:35 +0200

    react jsx tests

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
