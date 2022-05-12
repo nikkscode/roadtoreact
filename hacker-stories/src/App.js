225675eebe279bfa626a97672d000a984bbc61cd
nikkscode <dominik.kisiala@gmail.com>
2022-05-12 16:14:51 +0200

    Lists in React

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -5,6 +5,25 @@ const welcome = {
   title: 'React',
 }
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 function App() {
   return (
     <div>
@@ -12,7 +31,19 @@ function App() {
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      {list.map(function(item){
+      return <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title} </a>
+        </span>
+        <span>{item.author} </span>
+        <span>{item.num_comments} </span>
+        <span>{item.points} </span>
+        </div>;
+    })}  
     </div>
+    
   );
 }
 
